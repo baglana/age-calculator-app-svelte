fd93b425ca08f31692a79cd2aa26e8b4b6e5929c
baglana <102351104+baglana@users.noreply.github.com>
2024-02-22 19:02:52 +0600

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    let dobDay: number;
+    let dobMonth: number;
+    let dobYear: number;
+
+    $: dob = new Date(dobYear, dobMonth - 1, dobDay);
+    $: years = new Date(Date.now() - dob.getTime()).getUTCFullYear() - 1970;
+    $: months = new Date(Date.now() - dob.getTime()).getMonth();
+    $: days = new Date(Date.now() - dob.getTime()).getDate();
+</script>
+
+<main>
+    <form>
+        <label for="day">
+            DAY
+            <input bind:value={dobDay} name="day" type="number">
+        </label>
+        <label for="month">
+            MONTH
+            <input bind:value={dobMonth} name="month" type="number">
+        </label>
+        <label for="year">
+            YEAR
+            <input bind:value={dobYear} name="year" type="number">
+        </label>
+
+        <output>
+            {#if years}
+                <p>{years} years</p>
+            {/if}
+            {#if months}
+                <p>{months} months</p>
+            {/if}
+            {#if days}
+                <p>{days} days</p>
+            {/if}
+        </output>
+    </form>
+</main>
